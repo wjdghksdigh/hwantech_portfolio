d3226a6f8e3657f06386c9c1078e43d8b1cf5c61
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-10-19 00:02:24 +0900

    상단바 변경

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
             <a className="mr-5 hover:text-gray-900">AboutMe</a>
             <a className="mr-5 hover:text-gray-900">Skills</a>
             <a className="mr-5 hover:text-gray-900">Project</a>
-            <a className="mr-5 hover:text-gray-900">Contact</a>
+            <a className="mr-5 hover:text-gray-900">ContactMe</a>
             </nav>
             <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
