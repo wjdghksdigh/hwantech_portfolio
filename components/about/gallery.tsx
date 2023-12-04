9511a64d9cd321d65911840090d080508a1efbbe
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-12-04 14:36:16 +0900

    액빡 사진 수정

--- components/about/gallery.tsx
+++ components/about/gallery.tsx
@@ -48,7 +48,7 @@ const Gallery = () => {
               <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
-                src="/assets/images/gallery04.jpg"
+                src="/assets/images/gallery04.JPG"
                 width={502}
                 height={302}
               />
@@ -57,7 +57,7 @@ const Gallery = () => {
               <Image
                 alt="gallery"
                 className="w-full object-cover h-full object-center block"
-                src="/assets/images/gallery05.jpg"
+                src="/assets/images/gallery05.JPG"
                 width={503}
                 height={303}
               />
