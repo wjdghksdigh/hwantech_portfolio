1c6f4bc1ecd6df4282bab7b1eac0ff03487ec9dc
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-12-04 13:33:46 +0900

    header css 수정

--- components/footer/verticalLine.tsx
+++
@@ -1,5 +0,0 @@
-const VerticalLine = () => {
-  return <div className="my-0 mx-auto h-[90px] w-[1px] bg-gray-600"></div>;
-};
-
-export default VerticalLine;
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -23,15 +23,26 @@ const Header = () => {
   }, [scrollY]);
 
   return (
-      <div
-        className={`items-center shadow-bottom fixed z-10 flex w-full justify-center border-gray-400 px-16 py-8 backdrop-blur transition duration-500 ease-in-out ${
-          show ? '' : 'transparent'
-        }`}
-      >
-      <Link href="/">
-        <div className="text-baby-sky"> hwantech </div>
-      </Link>
-      <div className="inline-flex items-center justify-center sm:ml-auto">
+    //   <div
+    //     className={`items-center shadow-bottom fixed z-10 flex w-full justify-center border-gray-400 px-16 py-8 backdrop-blur transition duration-500 ease-in-out ${
+    //       show ? '' : 'transparent'
+    //     }`}
+    //   >
+    //   <Link href="/">
+    //     <div className="text-baby-"> hwantech </div>
+    //   </Link>
+    //   <div className="inline-flex items-center justify-center sm:ml-auto">
+    //     <HeaderButton name="Home" />
+    //     <HeaderButton name="About" />
+    //     <HeaderButton name="Projects" />
+    //     <a href="https://open.kakao.com/o/s88qeANf" className="px-4" target="_blank"> Contact </a>
+    //     <DarkModeToggleButton />
+    //   </div>
+    // </div>
+
+  <header className="flex items-center justify-center text-gray-600 body-font bg-white ">
+    <div className="container mx-auto fixed flex flex-wrap p-4 flex-col md:flex-row w-[500px] mt-24 z-1 border items-center border-gray-100 shadow-lg text-center rounded-3xl">
+      <div className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
         <HeaderButton name="Home" />
         <HeaderButton name="About" />
         <HeaderButton name="Projects" />
@@ -39,6 +50,9 @@ const Header = () => {
         <DarkModeToggleButton />
       </div>
     </div>
+  </header>
+
+
   );
 };
 
