b933291b45b5b193e40de9bbebc9ace970a577f0
HwanTech <78897615+wjdghksdigh@users.noreply.github.com>
2023-12-04 13:41:34 +0900

    header css 재수정

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <div className="text-gray-600 body-font bg-white border-gray-400 shadow-topx">
-        <div className="container bg-white px-6 py-7 mx-auto md:justify-end justify-center flex items-center sm:flex-row flex-col">
+        <div className="container bg-white px-6 py-5 mx-auto md:justify-end justify-center flex items-center sm:flex-row flex-col">
           <div className="flex title-font font-medium items-center justify-center text-gray-900">
             <Link href="/"><span className="ml-3 text-xl">hwantech</span></Link>
           </div>
